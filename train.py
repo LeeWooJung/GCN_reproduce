e33678a6ba7fed55f41fe35780997920ed789dd9
LeeWooJung <wjlee612@gmail.com>
2020-03-10 18:08:33 +0900

    Change code

--- train.py
+++ train.py
@@ -1,5 +1,5 @@
 import numpy as np
 from preprocessing import load_data
 
-adj, features, labels, idx_train, idx_val, idx_test = load_data('cora', 'citation')
+features, adj = load_data('citeseer', 'citation')
 
